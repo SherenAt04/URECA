728ec663433bc8e067d26099b5f39d1d0d677a09
Sheren <sheren.at04@gmail.com>
2024-12-30 22:06:39 +0700

    update yey

--- crack_bem_pert_H.jl
+++ crack_bem_pert_H.jl
@@ -9,8 +9,10 @@
 # normal, strike-slip, and dip-slip vectors
 # Group all of the properties to a matrix
 # Use the matrix for Green's functions and stress calculation
+using LinearAlgebra
 
 function crack_bem_pert_H(L, mu, nu, x, y)
+
     # inputs:
     # L = length of fault
     # mu = shear modulus
@@ -50,7 +52,14 @@ function crack_bem_pert_H(L, mu, nu, x, y)
     N = length(xc)
 
     # initialise normal, strike-slip, and dip-slip vectors
-    vn = zeros(3,N) # normal vectors
+    #vn = [cosd.(patch_strike) .* sind.(patch_dip); 
+   # -sind.(patch_strike) .* sind.(patch_dip); 
+   # cosd.(patch_dip)]
+    vn = [
+        cosd(patch_strike[1]) * sind(patch_dip[1]) cosd(patch_strike[2]) * sind(patch_dip[2]) cosd(patch_strike[3]) * sind(patch_dip[3])
+        -sind(patch_strike[1]) * sind(patch_dip[1]) -sind(patch_strike[2]) * sind(patch_dip[2]) -sind(patch_strike[3]) * sind(patch_dip[3]) 
+        cosd(patch_dip[1]) cosd(patch_dip[2]) cosd(patch_dip[3])
+    ]
     vs = zeros(3,N) # strike-slip vectors
     vd = zeros(3,N) # dip-slip vectors
 
@@ -65,21 +74,24 @@ function crack_bem_pert_H(L, mu, nu, x, y)
         # x-component = cos(strike) sin(dip)
         # y-component = -sin(strike) sin(dip)
         # z-component = cos(dip)
-        vn[:, iN] = normalize([cosd(patch_strike[iN]) * sind(patch_dip[iN])
-        - sind(patch_strike[iN]) * sind(patch_dip[iN]) cosd(patch_dip[iN])])
+        vn[:, iN] .= vn[:, iN] / norm(vn[:, iN])
         # variable vn will points outward, perpendicular to the fault plane which represents the fault plane's 
         # orientation
 
         # put the strike-slip vector to the fault plane (perpendicular to the normal vector)
         # take x and y components of the normal vector 
         # then rotate the 2D vector by 90 degrees counterclockwise
-        vs[1:2, iN] = [0 1; -1 0] * vn[1:2, iN] # rotate 90 degrees counterclockwise
-        vs[:, iN] = -normalize(vs[:, iN]) # normalise and invert direction
+        # rotate 90 degrees counterclockwise
+        vs[1:2, iN] .= [0 1; -1 0] * vn[1:2, iN] 
+        # normalise and invert direction
+        vs[:, iN] .= -vs[:, iN] / norm(vs[:, iN]) 
         # variable vs lies along the fault strike direction which defines the horizontal 
         # slip direction for strike-slip motion
+        vn_vec = vn[:, iN]
+        vs_vec = vs[:, iN]
 
         # put the dip-slip vector to the fault plane (orthogonal to normal vector and strike-slip vector)
-        vd[:, iN] = normalize(cross(vn[:, iN], vs[:, iN])) # dip-slip vector
+        vd[:, iN] = cross(vn_vec, vs_vec) # dip-slip vector
         # variable vd lies along the fault dip direction which defines the vertical slip 
         # direction for the dip-slip motion 
 
@@ -92,7 +104,7 @@ function crack_bem_pert_H(L, mu, nu, x, y)
 m = zeros(10, N)
 
 # Assign values to the rows of m
-m[1, :] .= lstat
+m[1, :] .= ls
 m[2, :] .= W
 m[3, :] .= D
 m[4, :] .= patch_dip
@@ -109,42 +121,59 @@ GN = zeros(N, N)
 PU = zeros(N, N)
 
 # Create a vertical unit vector vss 
+# vss represents the vertical direction of the components
 vss = [0.0, 0.0, 1.0]
-for i in 1:N 
-    for i2 in 0:0
-        # Copy the i-th column of m 
-        m1 = copy(m[:,i]) 
-        m1[8 + i2] = 1 
 
-        # Compute stress tensor
-        Stress, _ = RDstressFS(
-            xloc[1,:], xloc[2,:], xloc[3,:], 
-            m1[6], m1[7], m1[3], m1[1], m1[2],
-            0, m1[4], m1[5], 90, 1, 0, mu, lambda, "Pc"
-        )
+    # Loop over each segment of the fault with N as the total number of segments
+    for i in 1:N 
 
-        # Rearrange stress matrix
-        S = permutedims(Stress[:, [1,4,5,2,6,3]])
+        # Set single iteration loop where;
+        for i2 in 0:0
+            # m1 will extract the i-th column of m 
+            # and the function copy will create new array to avoid modifying
+            # the original matrix m
+            m1 = copy(m[:,i]) 
 
-        # Compute traction vectors
-        Tn = [
-            sum(S[1:3, :] .* vn)
-            sum(S[[2, 4, 5], :] .* vn)
-            sum(S[[3, 5, 6], :] .* vn)
-        ]
+            # Set the 8th element of m1 as 1 
+            m1[8 + i2] = 1 
 
-        # Update the matrix 
-        G[:, 1] .= sum(Tn .* vss)
-        GN[:, i] .= sum(Tn .* vn)
-        PU[:, i] .= -(S[1,:] + S[4,:] + S[6,:]) / 3
+            # List stress components
+            # The inputs to RDstressFS are
+            # xloc[1:3, :] = 3D coordinates of the fault
+            # m1[6:1] = fault parameters
+            # mu, lambda = material properties 
+            # Pc = type of stress calculation
+            # The output will be matrix Stress that contain stress tensor components
+            # at specific coordinates
+            Stress, _ = RDstressFS(
+                xloc[1,:], xloc[2,:], xloc[3,:], 
+                m1[6], m1[7], m1[3], m1[1], m1[2],
+                0, m1[4], m1[5], 90, 1, 0, mu, lambda, "Pc"
+            )
+
+            # Rearrange stress matrix
+            # Function permutedims to transpose the rearranged stress tensor
+            S = permutedims(Stress[:, [1,4,5,2,6,3]])
+
+            # Compute traction vectors
+            # First project the stress components to vn then combines all the projections from all the coordinates
+            Tn = [
+                sum(S[1:3, :] .* vn)
+                sum(S[[2, 4, 5], :] .* vn)
+                sum(S[[3, 5, 6], :] .* vn)
+            ]
+
+            # Update the matrix 
+            # G contains the traction projected onto vertical direction
+            G[:, 1] .= sum(Tn .* vss)
+            # GN contains the traction projected onto fault-normal direction
+            GN[:, i] .= sum(Tn .* vn)
+            # PU stores the mean normal stress
+            PU[:, i] .= -(S[1,:] + S[4,:] + S[6,:]) / 3
 
 
-    end
-end 
-
-# i am so confused help lol
-disp(G,GN)
-
-# update the comments
+        end
+    end 
 
 end
+
