6f03445edf9d1cf5a58fdd902733a51bc2f30d95
Sheren <sheren.at04@gmail.com>
2025-01-02 23:09:49 +0700

    confusion

--- RDstressFS.jl
+++ RDstressFS.jl
@@ -1,4 +1,4 @@
-function RDstressFS(X, Y, Z, XO, YO, depth, L, W, pluge, dip, strike, rake, slip, opening, mu, lambda, RefPoint)
+function RDstressFS(X, Y, Z, X0, Y0, depth, L, W, plunge, dip, strike, rake, slip, opening, mu, lambda, RefPoint)
     # Poisson's ratio
     nu = lambda / (lambda + mu) / 2 
 
@@ -36,16 +36,56 @@ function RDstressFS(X, Y, Z, XO, YO, depth, L, W, pluge, dip, strike, rake, slip
     ]
 
     Rt = Rz2 * Ry * Rz1
-
+    @show Rt
+    
     # Coordinates for the points defining the fault
     Pt1 = [-W/2, L/2, 0]'
     Pt2 = [-W/2. - L/2, 0]'
     Pt3 = [W/2, - L/2, 0]'
     Pt4 = [W/2, L/2, 0]'
 
-    
+    # Set reference point based on the input argument 
+    if RefPoint == "Pc"
+        Ptr = [0, 0, 0]'
+    elseif RefPoint == "P1"
+        Ptr = [-W/2, L/2, 0]'
+    elseif RefPoint == "P2"
+        Ptr = [-W/2, - L/2, 0]'
+    elseif RefPoint == "P3"
+        Ptr = [W/2, - L/2, 0]'
+    elseif RefPoint == "P4"
+        Ptr = [W/2, L/2, 0]'
+    elseif RefPoint in ["mP12", "mP21"]
+        Ptr = [-W/2, 0, 0]'
+    elseif RefPoint in ["mP23", "mP32"]
+        Ptr = [0, - L/2, 0]'
+    elseif RefPoint in ["mP34", "mP43"]
+        Ptr = [W/2, 0, 0]'
+    elseif RefPoint in ["mP14", "mP41"]
+        Ptr = [0, L/2, 0]'
+    else
+        throw(ArgumentError("Undefined RD reference point!"))
+    end 
+
+    @show size(X0)
+    @show size(Y0)
+    @show size(depth)
+    @show size(Ptr)
+    @show size(Rt)
+
+    # Calculate the displacement 
+    Pr = [X0, Y0, -depth]' - Rt * Ptr
+    P1 = Rt * Pt1 + Pr 
+    P2 = Rt * Pt2 + Pr 
+    P3 = Rt * Pt3 + Pr 
+    P4 = Rt * Pt4 + Pr 
+
+    return P1, P2, P3, P4 
+
 
     
+
+
 
 
 end 
