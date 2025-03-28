3df0266c1d51c6302cacb2540557ff3ebac985b5
Sheren <sheren.at04@gmail.com>
2025-03-28 19:26:40 +0800

    try

--- CoordTrans.jl
+++ CoordTrans.jl
@@ -8,13 +8,9 @@ function CoordTrans(x1, x2, x3, A)
     x2 = vec(x2)
     x3 = vec(x3)
 
-    mat = [
-        x1'
-        x2'
-        x3'
-    ]
+    mat = hcat(vec(x1), vec(x2), vec(x3))
 
-    r = A * mat 
+    r = mat * A' 
     X1 = r[1,:]'
     X2 = r[2, :]'
     X3 = r[3, :]'
